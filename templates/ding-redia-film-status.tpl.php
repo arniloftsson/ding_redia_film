41a342905fd41261f89edf567822bc36c86021e5
Arni Loftsson <arniloftsson10.com>
2022-06-27 12:46:50 +0000

    Fixed loan status

--- templates/ding-redia-film-status.tpl.php
+++ templates/ding-redia-film-status.tpl.php
@@ -8,20 +8,11 @@
  */
 ?>
 
-<div class="material-item odd">
-    <?php // @TODO: Hardcode 73%? ?>
-    <div class="chart" data-percent="73" data-scale-color="#ffb400">73%</div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-
-    <?php // @TODO: Not valid path ?>
-    <script src="/path/to/jquery.easy-pie-chart.js"></script>
-    <script>
-      <?php // @TODO: Why the inline javascript ?>
-      $(function() {
-            $('.chart').easyPieChart({
-                // your options goes here
-            });
-      });
-  </script>
-</div>
+<!-- <div class="redia-film-status">
+    <div class="redia-film-status-progress-bar">
+        <div class="redia-film-status-progress-bar-inner">
+            <span><?php print $user_loans?></a></span>
+        </div>
+    </div>
+</div> -->
 
