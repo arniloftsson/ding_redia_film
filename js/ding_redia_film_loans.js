4aebbcdd7839e7c1b27f34d94ee9c978be6e3ecd
Arni Loftsson <arniloftsson10.com>
2022-08-09 14:00:22 +0000

    Added count of film loans in menu

---
+++ js/ding_redia_film_loans.js
@@ -0,0 +1,23 @@
+(function ($) {
+  "use strict";
+  Drupal.behaviors.ding_redia_film = {
+    attach: function (context) {
+      $('ul.main-menu-third-level').once('redia-film-loan-count', function() {
+        var url = Drupal.settings.dingRediaFilm.url;
+        console.log(url);
+        $.getJSON(url, function (data) {
+          console.log(2);
+          console.log(data);
+          if (data !== null) {
+            $('ul.main-menu-third-level li a.menu-item').each(function (element) {
+              if ($(this).attr('href') == '/user/me/status-loans') {
+                $(this).html(data);
+              }
+            });
+          }
+        });
+      });
+    }
+  }
+
+}(jQuery));
